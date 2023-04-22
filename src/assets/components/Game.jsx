2599715fe1975077b0eb8f310620fe71abb96d48
Arthur dos Santos <arthur.santos@intelbras.com.br>
2023-04-22 15:37:37 -0300

    Verificação completa de vencedor e restart do game

--- src/assets/components/Game.jsx
+++ src/assets/components/Game.jsx
@@ -4,7 +4,7 @@ import { GameContainer, Squares } from "./styles";
 export function Game() {
     const [squares, setSquares] = useState([null,null,null,null,null,null,null,null,null]);
     const [playerX, setPlayerX] = useState(true);
-
+    const [winner, setWinner] = useState(false)
 
     const handleClick = (index) => {
 
@@ -12,17 +12,17 @@ export function Game() {
         newSquares[index] = playerX ? 'X' : 'O';
         setSquares(newSquares);
         setPlayerX(!playerX) // Alterna Player
-        console.log('Index aqui', index)
-        console.log(squares, "SQUARES NEW")
-        console.log(squares[1], "SQUARES 0")
-        checkWinner();
-        if(index[1] === 'X') {
-            console.log("estamos no if")
-        }
-        
+        if(winner == false) {
+            checkWinner();
+        }   
     }
 
-    const checkWinner = (squares) => {
+    const restartGame = () => {
+        setSquares([null,null,null,null,null,null,null,null,null]);
+        setWinner(false);
+    }
+
+    const checkWinner = () => {
         const lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -33,11 +33,16 @@ export function Game() {
             [0, 4, 8],
             [2, 4, 6]
         ]
-       for (let i = 0; i < lines.length; i++) {
-           
-       }
-        
 
+       for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            console.log([a, b, c])
+           if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                alert("VITORIA")
+                setWinner(true);
+                restartGame();
+           }
+       }
     }
     return(
         
