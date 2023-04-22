1cf4125feede618dfab717a8db6ff5aae6716e2e
ArthurSantos19 <54809333+ArthurSantos19@users.noreply.github.com>
2023-04-22 00:52:26 -0300

    Iniciando validação de vencedor

--- src/assets/components/Game.jsx
+++ src/assets/components/Game.jsx
@@ -11,7 +11,33 @@ export function Game() {
         const newSquares = [...squares];
         newSquares[index] = playerX ? 'X' : 'O';
         setSquares(newSquares);
-        setPlayerX(!playerX) // ALTERNA PLAYER
+        setPlayerX(!playerX) // Alterna Player
+        console.log('Index aqui', index)
+        console.log(squares, "SQUARES NEW")
+        console.log(squares[1], "SQUARES 0")
+        checkWinner();
+        if(index[1] === 'X') {
+            console.log("estamos no if")
+        }
+        
+    }
+
+    const checkWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+       for (let i = 0; i < lines.length; i++) {
+           
+       }
+        
+
     }
     return(
         
